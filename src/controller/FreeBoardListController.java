c3ea5c67e05aab8cd36fbdab87046c3374544cec
nerei <nerei@LAPTOP-1L9TF4VL>
2019-11-18 19:18:41 +0900

    주석추가, 연습

--- src/controller/FreeBoardListController.java
+++ src/controller/FreeBoardListController.java
@@ -17,7 +17,7 @@ public class FreeBoardListController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		//헤헤
 	}
 
 }
