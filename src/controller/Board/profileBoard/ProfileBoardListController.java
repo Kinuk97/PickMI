5c213fa568583bea61586c8be4866678e0a3bcd2
dongsan <nereid1007@naver.com>
2019-11-22 15:19:40 +0900

    폴더수정 및 profileBoardDto 생성

--- src/controller/Board/profileBoard/ProfileBoardListController.java
+++ src/controller/Board/profileBoard/ProfileBoardListController.java
@@ -14,7 +14,8 @@ public class ProfileBoardListController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		
+		System.out.println("test");
 	
 	}
 }
