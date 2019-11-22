3b5e30c01cfd645b4d34ff00c9fb434b64ee2058
ubermansh <wbhblb@gmail.com>
2019-11-22 10:28:29 +0900

    마이페이지 생성 마이페이지, 회원정보수정 페이지 controller, jsp 생성

---
+++ src/controller/mypage/MyInfoListController.java
@@ -0,0 +1,27 @@
+package controller.mypage;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyInfoListController
+ */
+@WebServlet("/mypage/info")
+public class MyInfoListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		req.getRequestDispatcher("/WEB-INF/views/mypage/info.jsp")
+		.forward(req, resp);
+		
+		
+	}
+	
+}
