8265d231b3b9e196111e47d7c0acb6b4a49a8b5d
ubermansh <wbhblb@gmail.com>
2019-12-06 16:16:47 +0900

    비밀번호수정, 프로필사진 수정, 회원탈퇴(모달안돼서 알러트로구현), 관리자회원탈퇴 구현

--- src/controller/mgr/MgrUserDelete.java
+++ src/controller/mgr/MgrUserDelete.java
@@ -7,11 +7,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dto.User;
+import serivce.face.FileService;
 import serivce.face.MgrService;
+import serivce.face.MyPageService;
+import serivce.impl.FileServiceImpl;
 import serivce.impl.MgrServiceImpl;
+import serivce.impl.MyPageServiceImpl;
 
 /**
  * Servlet implementation class MgrUserDelete
@@ -19,15 +22,24 @@ import serivce.impl.MgrServiceImpl;
 @WebServlet("/mgr/user/delete")
 public class MgrUserDelete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+	private FileService fileService = FileServiceImpl.getInstance();
+	private MyPageService myPageService = MyPageServiceImpl.getInstance();
 	// ManagerService 객체
 	private MgrService mgrService = MgrServiceImpl.getInstance();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
+		String[] strings = req.getParameterValues("checkRow");
+		User user = new User();
 		
+		for(String string : strings) {
+			user.setUserno(Integer.parseInt(string));
+			fileService.myPhotoDelete(req.getServletContext().getRealPath("upload"), user); 
+			myPageService.userDelete(user);
+
+		}
+		resp.sendRedirect("/mgr/userlist");
 	}
 	
 }
