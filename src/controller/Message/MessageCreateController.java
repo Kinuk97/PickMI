1f96502bab68f064dce522263c4bdcfa30e319a8
godnj <godnj@DESKTOP-3UAVRR3>
2019-12-07 19:24:52 +0900

    메세지 주석처리

--- src/controller/Message/MessageCreateController.java
+++ src/controller/Message/MessageCreateController.java
@@ -31,19 +31,15 @@ public class MessageCreateController extends HttpServlet {
 		chatter1.setChat_user((Integer) req.getSession().getAttribute("userno")); //로그인한 유저의 유저번호
 		chatter2.setChat_user(Integer.parseInt(req.getParameter("userno"))); //채팅을 나눌 사용자의 유저번호
 		
-		System.out.println(1);
-		
-		System.out.println("chatter1 : " + chatter1);
-		System.out.println("chatter2 : " + chatter2);
+//		System.out.println("chatter1 : " + chatter1);
+//		System.out.println("chatter2 : " + chatter2);
 		
 		int chatno = messageService.chattingUserCheck(chatter1, chatter2);
 		
 		if (chatno == 0) {
-			System.out.println(2);
 			System.out.println("검색한 사용자와 메세지 생성 중 오류");
 		}
 		
-		System.out.println(4);
 		resp.getWriter().println("{\"chat_no\":"+chatno+"}");
 		
 	}
